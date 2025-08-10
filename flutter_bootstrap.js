f7402ecdf5328ec49d463685fa01ac9968561f63
gerlanss <gerlan.ss@hotmail.com>
2025-08-10 18:20:48 -0400

    UI: fundo cinza claro e pizza primeiro

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "976864662"
+    serviceWorkerVersion: "3953215760"
   }
 });
