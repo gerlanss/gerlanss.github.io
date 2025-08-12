68bc97bf6c12560f8a9ffa87323f9d6859ca2398
gerlanss <gerlan.ss@hotmail.com>
2025-08-11 20:00:53 -0400

    Semana X de Y com destaque

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "56408675"
+    serviceWorkerVersion: "1499359894"
   }
 });
