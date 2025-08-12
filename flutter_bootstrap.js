ed4a5dc93ffde8e078647f549cc1f9174aa1f148
gerlanss <gerlan.ss@hotmail.com>
2025-08-11 21:17:37 -0400

    Dashboard: implementar lógica de semanas baseada em sábados

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3955386370"
+    serviceWorkerVersion: "3181625027"
   }
 });
